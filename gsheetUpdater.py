9c57a35c99c2baa350b95c34700c50daa816c520
Jordan Tom <jtom@quantcast.com>
2019-08-25 10:45:17 -0700

    added exception handling on the updaterecord method

--- gsheetUpdater.py
+++ gsheetUpdater.py
@@ -26,20 +26,23 @@ def find_last_row(column_num, worksheet):
     return len(rows)
 
 def update_record(sensor_num ,time,temp,humidity):
-    
-    if sensor_num == 4:
-        worksheet = open_worksheet()
-        empty_row = find_last_row(1,worksheet) + 1
-    
-        worksheet.update_cell(empty_row, 1, time)
-        worksheet.update_cell(empty_row, 2, temp)
-        worksheet.update_cell(empty_row, 3, humidity)
+    try:
+        if sensor_num == 4:
+            worksheet = open_worksheet()
+            empty_row = find_last_row(1,worksheet) + 1
+        
+            worksheet.update_cell(empty_row, 1, time)
+            worksheet.update_cell(empty_row, 2, temp)
+            worksheet.update_cell(empty_row, 3, humidity)
 
 
-    if sensor_num == 17:
-        worksheet = open_worksheet()
-        empty_row = find_last_row(5,worksheet) + 1
-    
-        worksheet.update_cell(empty_row, 5, time)
-        worksheet.update_cell(empty_row, 6, temp)
-        worksheet.update_cell(empty_row, 7, humidity)
+        if sensor_num == 17:
+            worksheet = open_worksheet()
+            empty_row = find_last_row(5,worksheet) + 1
+        
+            worksheet.update_cell(empty_row, 5, time)
+            worksheet.update_cell(empty_row, 6, temp)
+            worksheet.update_cell(empty_row, 7, humidity)
+    except:
+        print "Google login API issue"
+        pass
